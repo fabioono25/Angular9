3a48b50001a680ee1568ea670eb689c0af0bc793
fabioono25 <fabioono25@gmail.com>
2020-04-25 23:15:56 +0100

    recipes

--- FoodWeb/src/app/recipes/recipe-list/recipe-list.component.ts
+++ FoodWeb/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Pasta 1', 'Just a pasta 1', 'https://www.thespruceeats.com/thmb/N-MHztL8s2Ld1L1ivCXclMYOor4=/2000x1500/filters:fill(auto,1)/GettyImages-885397466-5c0cc0634cedfd00012758a7.jpg'),
+    new Recipe('Pasta 2', 'Just a pasta 2', 'https://www.foodiecrush.com/wp-content/uploads/2019/07/Pomodoro-Sauce-foodiecrush.com-015-683x1024.jpg'),
+    new Recipe('Pasta 3', 'Just a pasta 3', 'https://www.gimmesomeoven.com/wp-content/uploads/2018/02/Creamy-Rose%CC%81-Pasta-with-Roasted-Tomatoes-1-1100x1650.jpg')
+  ];
 
   constructor() { }
 
