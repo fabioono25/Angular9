d0484485cb64f3ad46d49d96a9f536a0788a3c51
fabioono25 <fabioono25@gmail.com>
2020-04-25 23:28:53 +0100

    Shopping List

--- FoodWeb/src/app/shopping-list/shopping-list.component.ts
+++ FoodWeb/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Pasta', 10),
+    new Ingredient('Salt', 2),
+    new Ingredient('Olive Oil', 1),
+    new Ingredient('Tomatoes', 5)
+  ];
 
   constructor() { }
 
