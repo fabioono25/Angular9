8c397ea99f89c80ea4aec5a77c363d1777df5b2a
fabioono25 <fabioono25@gmail.com>
2020-04-26 09:36:30 +0100

    add navigation

--- FoodWeb/src/app/app.component.ts
+++ FoodWeb/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FoodWeb';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    // console.log(`${feature}`);
+    this.loadedFeature = feature;
+  }
 }
