cd367e3c5cbce10798db06e0f4abfe8be34fe7e2
fabioono25 <fabioono25@gmail.com>
2020-04-26 10:10:32 +0100

    add ingredients feature

--- FoodWeb/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ FoodWeb/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', { static: false })
+  nameInputRef: ElementRef;
+
+  @ViewChild('amountInput', { static: false })
+  amountInputRef: ElementRef;
+
+  // ingredientAdded = new EventEmitter<{name: string, amount: number}>()
+  @Output()
+  ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem() {
+    const name = this.nameInputRef.nativeElement.value;
+    const amount = this.amountInputRef.nativeElement.value;
+
+    const newIngredient = new Ingredient(name, amount);
+
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
