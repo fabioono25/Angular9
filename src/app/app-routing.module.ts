7743b2c1ecb26d3a9dfeea61c0cd38afcdc732e8
Fabio Ono Tavoloni <fabioono25@gmail.com>
2021-02-02 15:45:22 +0000

    adding authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './auth/auth.guard';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -11,7 +12,9 @@ import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent, children: [
+  { path: 'recipes', component: RecipesComponent,
+    canActivate: [AuthGuard],
+    children: [
     { path: '', component: RecipeStartComponent},
     { path: 'new', component: RecipeEditComponent },
     { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService]},
